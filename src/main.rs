d3e80324f3f5a44b9a829272c9313290be7f3f3f
jakobdannel <jakob.dannel@t-online.de>
2022-10-10 17:43:39 +0200

    Set the image background to a grey color

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use image::{Rgb, RgbImage};
 
 fn main() {
-    const WIDTH: u32 = 500;
-    const HEIGHT:u32 = 500;
+    const WIDTH: u32 = 360;
+    const HEIGHT:u32 = 240;
     let mut img = RgbImage::new(WIDTH, HEIGHT);
 
-    img.put_pixel(250, 250, Rgb([255,255,255]));
+    for i in 0..WIDTH {
+        for j in 0..HEIGHT {
+            img.put_pixel(i, j, Rgb([51,51,51]));
+        }
+    }
+    
 
     img.save("./output/output.png").expect("Writing image to png");
 }
