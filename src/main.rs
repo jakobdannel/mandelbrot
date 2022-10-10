7928da58f6cef6568531036ef75115ec641a78d4
jakobdannel <jakob.dannel@t-online.de>
2022-10-10 17:44:19 +0200

    Formatting

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,16 @@ use image::{Rgb, RgbImage};
 
 fn main() {
     const WIDTH: u32 = 360;
-    const HEIGHT:u32 = 240;
+    const HEIGHT: u32 = 240;
+
     let mut img = RgbImage::new(WIDTH, HEIGHT);
 
     for i in 0..WIDTH {
         for j in 0..HEIGHT {
-            img.put_pixel(i, j, Rgb([51,51,51]));
+            img.put_pixel(i, j, Rgb([51, 51, 51]));
         }
     }
-    
 
-    img.save("./output/output.png").expect("Writing image to png");
+    img.save("./output/output.png")
+        .expect("Writing image to png");
 }
