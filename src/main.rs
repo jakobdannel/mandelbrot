d642311b79fa6e1e5a06ad262e69f956bd787b03
jakobdannel <jakob.dannel@t-online.de>
2022-10-10 17:37:56 +0200

    Added simple image rendering

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use image::{Rgb, RgbImage};
+
 fn main() {
-    println!("Hello, world!");
+    const WIDTH: u32 = 500;
+    const HEIGHT:u32 = 500;
+    let mut img = RgbImage::new(WIDTH, HEIGHT);
+
+    img.put_pixel(250, 250, Rgb([255,255,255]));
+
+    img.save("./output/output.png").expect("Writing image to png");
 }
